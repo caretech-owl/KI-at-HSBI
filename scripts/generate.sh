60a970684109f9ef433338395917ab2d3ba2a8b7
Alexander Neumann <aleneum@gmail.com>
2023-11-22 23:37:04 +0100

    add generate script

---
+++ scripts/generate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd ${SCRIPT_DIR}/..
+
+jupyter nbconvert --output-dir=_site/ --to html --no-prompt --no-input index.ipynb
+jupyter nbconvert --output-dir=_site/ --to slides --reveal-prefix=reveal slides/*.ipynb

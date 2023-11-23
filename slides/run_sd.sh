3e9a677321a99ad655f2fe07c82f542bd16fa78f
Alexander Neumann <aleneum@gmail.com>
2023-11-23 12:55:52 +0100

    remove uneeded stuff

--- slides/run_sd.sh
+++
@@ -1,8 +0,0 @@
-export COMMANDLINE_ARGS="--xformers --share --gradio-auth $2:$3"
-export TRANSFORMERS_CACHE="$1/.cache/hf" 
-export PIP_CACHE_DIR="$1/.cache/pip"
-
-mkdir -p $1/.cache
-
-cd $1
-bash ./webui.sh

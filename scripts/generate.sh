bf954263eb23dc8fdc3add069d1c9745e3f433e4
Alexander Neumann <aleneum@gmail.com>
2023-11-23 01:05:19 +0100

    embed images; remove imaging folder;

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd ${SCRIPT_DIR}/..
 
 jupyter nbconvert --output-dir=_site/ --to html --no-prompt --no-input index.ipynb
-jupyter nbconvert --output-dir=_site/ --to slides --reveal-prefix=reveal slides/*.ipynb
+jupyter nbconvert --output-dir=_site/ --to slides --embed-images --reveal-prefix=reveal slides/*.ipynb

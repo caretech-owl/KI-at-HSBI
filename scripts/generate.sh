26d8be82246532016dadc3b72f88e01ea539d899
Alexander Neumann <aleneum@gmail.com>
2023-11-24 13:13:23 +0100

    do not embed slides

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd ${SCRIPT_DIR}/..
+cp -r slides/images _site
 
 jupyter nbconvert index.ipynb \
     --output-dir=_site/ \
@@ -14,6 +15,5 @@ jupyter nbconvert slides/*.ipynb \
     --output-dir=_site/ \
     --to slides \
     --no-prompt \
-    --embed-images \
     --reveal-prefix reveal \
     --SlidesExporter.reveal_theme hsbi-cto

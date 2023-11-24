f9dbf4775858c5fa0d3af9b139f0034edab86625
Alexander Neumann <aleneum@gmail.com>
2023-11-24 16:03:59 +0100

    make index page static

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -5,12 +5,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd ${SCRIPT_DIR}/..
 cp -r slides/images _site
 
-jupyter nbconvert index.ipynb \
-    --output-dir=_site/ \
-    --to html \
-    --no-prompt \
-    --no-input
-
 jupyter nbconvert slides/*.ipynb \
     --output-dir=_site/ \
     --to slides \
